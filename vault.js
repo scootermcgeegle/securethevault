970d693f4e41ed4e2015885893d791ce0b13fa03
Scott Galen <scottcharlesgalen@yahoo.com>
2024-11-04 10:35:19 -0800

    committing changes to Vault exercise

---
+++ vault.js
@@ -0,0 +1,26 @@
+/*
+The combination of the lock is 10 - 40 - 39
+Create an equation that equals 10
+Create an equation that equals 40
+Create an equation that equals 39
+Show alert of combination
+*/
+
+// the Welcome message
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+ 
+  
+// equation total equals 10
+const num1 = 5 + 5;
+
+// equation total equals 40
+const num2 = 20 + 20;
+
+// equation total equals 39
+const num3 = 40 - 1;
+
+// sums of all the numbers
+let combination = num1 + "-" + num2 + "-" + num3
+
+// addition of three numbers displays the combination
+alert("This is your code " + combination);
